6eaed2b4760b351623b58d85c1f408d0f7236b82
Mike Kelly <mike@membervault.co>
2021-08-16 17:53:01 -0700

    docker mysql

--- setup.sh
+++ setup.sh
@@ -1,3 +1,16 @@
 # check selinux 
 # Edit the /etc/selinux/config file as follows:
 # SELINUX=permissive
+
+sudo dnf install docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo docker pull mysql/mysql-server:5.7
+
+sudo docker run --name=mysql1 --mount type=bind,src=/home/mkelly/Docker/mysql,dst=/var/lib/mysql -d mysql/mysql-server:5.7
+
+sudo docker logs mysql1 2>&1 | grep GENERATED
+
+# Then Run
+# ALTER USER 'root'@'localhost' IDENTIFIED BY 'n3wj3rs3y';
