d38b0f1db07b73ce62f457335794f7414dd54e9f
Mike Kelly <mike@membervault.co>
2021-12-04 11:09:01 -0800

    silverblue updates

--- silverblue.sh
+++ silverblue.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #
-# Extension: https://extensions.gnome.org/extension/3843/just-perfection/
 
 
 gsettings set org.gnome.desktop.privacy remember-recent-files false
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 
-sudo rpm-ostree install gnome-tweaks papirus-icon-theme
 
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install org.gtk.Gtk3theme.Adwaita-dark
+
+sudo rpm-ostree install nodejs
