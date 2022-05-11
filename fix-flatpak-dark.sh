a632cc4c2c0fad028336cfd3a921e7650576e81a
Mike Kelly <mike@membervault.co>
2022-05-11 14:05:23 -0700

    add notes

--- fix-flatpak-dark.sh
+++ fix-flatpak-dark.sh
@@ -2,3 +2,8 @@ mkdir ~/.themes
 cp -R /usr/share/themes/Adwaita-dark ~/.themes/
 sudo flatpak override --filesystem=$HOME/.themes
 sudo flatpak override --env=GTK_THEME=Adwaita-dark
+
+# Notes:
+#   - Be sure to also log an issue with any app that's not respecting the dark theme, so they can fix it as well.
+#   - To view all the flatpak overrides you have, you can run `flatpak override --show`
+#   - To revert back to the stock theme, you can run `sudo flatpak override --reset`
